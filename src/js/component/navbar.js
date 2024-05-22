377db110d3e159ce755307f3a000adcd21df5459
Anibal-JPG <109487916+Anibal-JPG@users.noreply.github.com>
2024-05-22 19:03:39 +0000

    navbar listo

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faSortDown } from '@fortawesome/free-solid-svg-icons';
+
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light  mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<div className="p-3"><img className="" src="https://logowik.com/content/uploads/images/528_star_wars.jpg" width = "100px" height = "70px"/></div>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+				<div className="btn-group p-3">
+				  	<button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+				  	  Favorites
+				  	</button>
+				  	<ul style={{marginLeft: "-40px"}} className="dropdown-menu dropdown-menu-end dropdown-menu-lg-start">
+				  	  <li><a className="dropdown-item" href="#">Menu item</a></li>
+				  	  <li><a className="dropdown-item" href="#">Menu item</a></li>
+				  	  <li><a className="dropdown-item" href="#">Menu item</a></li>
+				  	</ul>
+				</div>
 				</Link>
 			</div>
 		</nav>
